ee53ab4c35a045cbe5566d2afbf70a11f6274ddc
Sanjana M <sanjanamurugesan06@gmail.com>
2024-05-04 17:41:35 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import ToggleButton from "./components/ToggleButton";
-import ConditionalRend from "./components/ConditionalRend";
-import Form from "./components/Form";
-import ErrorComponent from "./components/ErrorComponent";
+
+// Lazy-load components
+const Home = React.lazy(() => import("./components/Home"));
+const ToggleButton = React.lazy(() => import("./components/ToggleButton"));
+const ConditionalRend = React.lazy(() => import("./components/ConditionalRend"));
+const Form = React.lazy(() => import("./components/Form"));
+const ErrorComponent = React.lazy(() => import("./components/ErrorComponent"));
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route exact path="/" element={<Home/>}></Route>
-        <Route exact path="/task2" element={<ToggleButton/>}></Route>
-        <Route exact path="/task3" element={<ConditionalRend/>}></Route>
-        <Route exact path="/task4" element={<Form></Form>}></Route>
-        <Route exact path="/task5" element={<ErrorComponent></ErrorComponent>}></Route>
-      </Routes>
+      
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            {/* Define routes with lazy-loaded components */}
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/task2" element={<ToggleButton />} />
+            <Route exact path="/task3" element={<ConditionalRend />} />
+            <Route exact path="/task4" element={<Form />} />
+            <Route exact path="/task5" element={<ErrorComponent />} />
+          </Routes>
+        </Suspense>
+      
     </div>
   );
 }
